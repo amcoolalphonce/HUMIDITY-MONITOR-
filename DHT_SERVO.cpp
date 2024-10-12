0f518f97a2b8da320b5041833cb642e414290e09
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-10-12 22:52:30 +0300

    Update DHT_SERVO.cpp

--- DHT_SERVO.cpp
+++ DHT_SERVO.cpp
@@ -11,3 +11,4 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define DHT_SENSOR_TYPE DHT22
+DHT dht_sensor(DHT_PIN, DHT_SENSOR_TYPE); // Create  DHT sensor object
