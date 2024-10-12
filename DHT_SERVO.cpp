4ce3e9f4f807c3fb3e05ab59639b10f372f8c035
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-10-12 22:48:50 +0300

    required libraries

---
+++ DHT_SERVO.cpp
@@ -0,0 +1,13 @@
+#include <DHT.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <WiFiClientSecure.h>
+#include "secrets.h"
+#include "pin_configurations.h"
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define DHT_SENSOR_TYPE DHT22
