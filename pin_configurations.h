f148d12dfdd96397b4f2c3e867f254f5ee82a5e5
amcool <82672024+amcoolalphonce@users.noreply.github.com>
2024-10-13 22:24:57 +0300

    pin configs

--- pin_configurations.h
+++ pin_configurations.h
@@ -1,1 +1,12 @@
-
+#define PUSH_BUTTON_PIN 4 //A1
+#define PUSH_BUTTON_PIN 40  //D1
+#define WATER_LEVEL_PIN 5 //A2
+#define LDR_PIN 6 //A3
+#define DHT_PIN 7 //A4
+#define ONBOARD_LED_PIN 9 
+#define BUZZER_PIN 15 //A5
+#define INA_PIN 38  //D3
+#define INB_PIN 35  //D4
+#define MQ2_PIN 34  //A8
+#define PIR_PIN 39  //D2
+#define SERVO_PIN 20 //A10
